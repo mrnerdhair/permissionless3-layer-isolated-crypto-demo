346fd483ac39f2f175061d7780de93612182e262
Reid Rankin <reidrankin@gmail.com>
2024-10-08 17:02:23 -0600

    output format tweaks

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,8 +1,6 @@
 #[allow(warnings)]
 mod bindings;
 
-use std::fmt::format;
-
 use crate::bindings::mrnerdhair::isolated_crypto;
 use crate::bindings::{Guest, Output, TaskQueueInput};
 
@@ -18,6 +16,7 @@ fn compressed_point_to_sec1(value: CompressedPoint) -> [u8; 33] {
 
 impl Guest for Component {
     fn run_task(request: TaskQueueInput) -> Output {
+        let message: String = String::from_utf8(request.request).or(Err("input must be valid UTF-8"))?;
         let mnemonic = isolated_crypto::mnemonic_provider::get_mnemonic()?;
         let seed = mnemonic.to_seed("TREZOR");
         let node = seed.to_master_key(None);
@@ -27,15 +26,15 @@ impl Guest for Component {
         let node = node.derive(0);
         let node = node.derive(0);
         let ecdsa_key = node.into_secp256k1_ecdsa_key();
-        let message = ["\x19Ethereum Signed Message:\n".as_bytes(), format!("{}", request.request.len()).as_bytes(),  &request.request].concat();
-        let (sig, rec_id) = ecdsa_key.sign(isolated_crypto::types::DigestAlgorithm256::Keccak256, &message, None);
+        let sign_bytes = ["\x19Ethereum Signed Message:\n".as_bytes(), format!("{}", message.len()).as_bytes(),  &message.as_bytes()].concat();
+        let (sig, rec_id) = ecdsa_key.sign(isolated_crypto::types::DigestAlgorithm256::Keccak256, &sign_bytes, None);
         let v = 27 + if !rec_id.contains(RecoveryId::IS_Y_ODD) { 0b00 } else { 0b01 } + if rec_id.contains(RecoveryId::IS_X_REDUCED) { 0b10 } else { 0b00 };
         let pk =  compressed_point_to_sec1(ecdsa_key.get_public_key());
         let address = k256::ecdsa::VerifyingKey::from_sec1_bytes(&pk).unwrap().to_encoded_point(false);
         let address = [address.x().unwrap().clone(), address.y().unwrap().clone()].concat();
         let address = Keccak256::digest(address.clone());
         let address = &address[12..32];
-        Ok(format!(r#"{{"sig": "{}{}{:02x}", "msg": "0x{}", "pk": "{}", "addr": "0x{}"}}"#, hex::encode(sig.r), hex::encode(sig.s), v, hex::encode(&message), hex::encode(&pk), hex::encode(address)).into())
+        Ok(format!(r#"{{"address": "0x{}", "msg": "{}", "sig": "0x{}{}{:02x}"}}"#, hex::encode(address), &message, hex::encode(sig.r), hex::encode(sig.s), v).into())
     }
 }
 
