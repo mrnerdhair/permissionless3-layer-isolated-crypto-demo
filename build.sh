8cfe8846ba7d065859fa64afbc29364c37dcf893
Reid Rankin <reidrankin@gmail.com>
2024-10-08 20:08:28 -0600

    commit build products to save wkg linkage headaches

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -xe
 cd "$(dirname "$0")"
-mkdir -p ./target
 
 pushd isolated-crypto
 ./build.sh
@@ -8,15 +7,5 @@ popd
 pushd demo
 ./build.sh
 popd
-
-wac plug \
-    --plug ./isolated-crypto/provider/target/wasm32-wasip1/release/isolated_crypto_provider.wasm \
-    ./demo/target/wasm32-wasip1/release/isolated_crypto_demo.wasm \
-    --output ./target/demo-no-engine.wasm
-
-wac plug \
-    --plug ./isolated-crypto/engine/target/wasm32-wasip1/release/isolated_crypto_engine.wasm \
-    ./target/demo-no-engine.wasm \
-    --output ./target/demo.wasm
 
 exec ./demo.sh "$@"
--- demo.sh
+++ demo.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 cd "$(dirname "$0")"
+mkdir -p ./target
 
 MSG="$1"
 [ -z "$MSG" ] && MSG='Example `personal_sign` message'
 
 [ -z "$MNEMONIC" ] && MNEMONIC="zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo vote"
+
+wac plug \
+    --plug ./isolated-crypto/provider/target/wasm32-wasip1/release/isolated_crypto_provider.wasm \
+    ./demo/target/wasm32-wasip1/release/isolated_crypto_demo.wasm \
+    --output ./target/demo-no-engine.wasm
+
+wac plug \
+    --plug ./isolated-crypto/engine/target/wasm32-wasip1/release/isolated_crypto_engine.wasm \
+    ./target/demo-no-engine.wasm \
+    --output ./target/demo.wasm
 
 avs-toolkit-cli wasmatic run --wasm-source ./target/demo.wasm \
     --envs "MNEMONIC=$MNEMONIC" \
